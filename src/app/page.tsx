a1b2d786c874a6fb6ba1c79ea18a0939a7021d97
Tarso Jabbes <tarsojabbes@gmail.com>
2024-01-10 17:50:28 -0300

    Add Home Page Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import ProductCard from '@/components/ProductCard/productCard'
+import HomeHeader from '@/components/HomeHeader/homeHeader'
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <HomeHeader 
+        companyLogo='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/VTEX_Logo.svg/1280px-VTEX_Logo.svg.png'
+        links={[{title: "Smartphones", redirectUrl:"/smartphones"}, 
+                {title: "Computers", redirectUrl:"/computers"}, 
+                {title: "Home care", redirectUrl:"/home-care"}]}/>
       <ProductCard 
         name='Radiador' 
         price={199.9} 
---
+++ src/components/HomeHeader/homeHeader.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image"
+import styles from "./homeHeader.module.css"
+import CartIcon from "../../../public/cart-icon.png"
+
+interface IHomeHeader {
+    links: IHomeHeaderLink[],
+    companyLogo: string
+}
+
+interface IHomeHeaderLink {
+    title: string,
+    redirectUrl: string
+}
+
+export default function HomeHeader(props: IHomeHeader) {
+    return(
+        <header className={styles.homeHeader}>
+            <Image 
+                src={props.companyLogo}
+                alt="Company's Logo"
+                width={100}
+                height={50}
+                style={{objectFit: "contain"}}
+
+            />
+
+            <div className={styles.linkSection}>
+            {props.links.map(link => 
+                                <a  className={styles.headerLink}
+                                    href={link.redirectUrl}>
+                                    {link.title}
+                                </a>)}
+            </div>
+
+            <div className={styles.utils}>
+                <a className={styles.cart}
+                    href='/login'>
+                    <Image
+                        src={CartIcon}
+                        alt="Cart icon"
+                        width={24}
+                        height={24}
+                        />
+                    <p>Cart</p>
+                </a>
+                <a href="/auth">
+                    Register or Log In
+                </a>
+            </div>
+        </header>
+    )
+}
