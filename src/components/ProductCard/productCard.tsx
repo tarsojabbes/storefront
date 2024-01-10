e0501d44a64a59964ab8a24ba69b23cedb0c92b3
Tarso Jabbes <tarsojabbes@gmail.com>
2024-01-10 17:00:47 -0300

    Add ProductCard component

---
+++ src/components/ProductCard/productCard.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import styles from './productCard.module.css'
+
+interface IProductCard {
+    name: string,
+    price: number,
+    imageUrl: string,
+    currency: string
+}
+
+export default function ProductCard(props: IProductCard) {
+    return (
+        <div className={styles.productCard}>
+            <Image
+                className={styles.productImage}
+                src={props.imageUrl}
+                alt="Product Image"
+                width={150}
+                height={120}
+            />
+            <h4 className={styles.productName}>
+                {props.name}
+            </h4>
+            <p className={styles.productPrice}>
+                {props.currency}{props.price}
+            </p>
+            <button className={styles.productButton}>
+                <a href='/produto'>Quero esse</a>
+            </button>
+        </div>
+    )
+}
