f5815acf3678cdbc85b629c65bd63ad4cfa19047
Tarso Jabbes <tarsojabbes@gmail.com>
2024-01-26 13:59:35 -0300

    Inline scripting for HotJar

--- src/app/hotjarScript.ts
+++
@@ -1,7 +0,0 @@
-
-import Hotjar from "@hotjar/browser"
-
-const siteId = 3842274
-const hotjarVersion = 6
-
-Hotjar.init(siteId, hotjarVersion)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,28 @@ import ProductsJson from "../../data/products.json"
 import Script from 'next/script'
 
 export default function Home() {
+  const siteId = 3842274
+  const hotjarVersion = 6
 
   return (
     <>
-    <Script src="./hotjarScript.ts" strategy='beforeInteractive'/>
+    <Script
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            console.log("I'm here");
+            (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:${siteId},hjsv:${hotjarVersion}};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `,
+        }}
+      />
+      
       <main className={styles.main}>
         <HomeHeader 
           companyLogo='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/VTEX_Logo.svg/1280px-VTEX_Logo.svg.png'
