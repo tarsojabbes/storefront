017f750c217fbf8da34ad9cad030cb9dcdc363a3
Tarso Jabbes <tarsojabbes@gmail.com>
2024-02-01 10:42:34 -0300

    Includes SmartLook Script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,14 +18,12 @@ export default function Home() {
         dangerouslySetInnerHTML={{
           __html: `
             console.log("I'm here");
-            (function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:${siteId},hjsv:${hotjarVersion}};
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            window.smartlook||(function(d) {
+              var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+              var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+              c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
+              })(document);
+              smartlook('init', 'cac6239e2de7625af4b8698f5a1f44841e517997', { region: 'eu' });
           `,
         }}
       />
